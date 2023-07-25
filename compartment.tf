a4e1283ff6f3f1ee3ee9145afe29bed02e6dd8f3
Aayush Sharma <aayusharma1401@gmail.com>
2023-07-26 04:26:04 +0530

    feat: add oke cluster in OCI

---
+++ compartment.tf
@@ -0,0 +1,7 @@
+
+resource "oci_identity_compartment" "tf-compartment" {
+    # Required
+    compartment_id = var.compartment_id
+    description = "Compartment for Terraform resources."
+    name = "test1_obsrv"
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "compartment_id" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "private_key_path" {}
+variable "fingerprint" {}
+variable "source_id" {}
+variable "subnet_id" {}
+variable "image_id" {} 
